a25c5f51f6044bef67d371b6ad430b648aa1eda3
Ian-Ribeiro <ianmaxrodrigues27@gmail.com>
2025-07-22 20:53:08 -0300

    Ian Ribeiro

---
+++ main.py
@@ -0,0 +1,8 @@
+from bson import ObjectId
+from models import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,21 @@
+from bson import ObjectId # Importando o ObjectId para gerar id's automaticamente.
+from datetime import datetime # Importando datetime que representa data e hora.
+from db import *
+
+db = get_database()
+
+# Criando o método de inserir.
+def inserir():
+    pass
+
+# Criando o método de consulta.
+def consulta():
+    pass
+
+# Criando o método de atualização.
+def atualizacao():
+    pass
+
+# Criando o método de remoção.
+def remocao():
+    pass
