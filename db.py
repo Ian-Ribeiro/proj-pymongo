c7afd9e4b3bc71c654abf5a482ab1733c8df0b53
Ian-Ribeiro <ianmaxrodrigues27@gmail.com>
2025-07-23 14:54:34 -0300

    Ian Ribeiro

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
+from pymongo.server_api import ServerApi
 from pymongo import MongoClient # Importando a classe MongoClient.
 from config import *
 
+client = MongoClient(uri,server_api=ServerApi('1'))
 # Conectando com o MongoDB Atlas.
-def get_database(uri:str,db_name:str):
-    client = MongoClient(uri)
+def get_database(db_name:str,client):
+    client = client
     return client[db_name]
--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from bson import ObjectId # Importando o ObjectId para gerar id's automaticamente.
 from datetime import datetime # Importando datetime que representa data e hora.
 from db import *
+from config import *
+from pprint import pprint
 
-db = get_database()
+db = get_database(db_name,client)
 
 # Criando o método de inserir.
 def inserir():
@@ -19,3 +21,21 @@ def atualizacao():
 # Criando o método de remoção.
 def remocao():
     pass
+
+def find():
+    colection_filmes = db['clientes']
+    query = {"nome": "Ana Silva"}
+
+    try:
+        filme = colection_filmes.find_one(query)
+        if filme:
+            print(" Client encontrado")
+            pprint(filme)
+        else:
+            print("Nenhum client encontrado!")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+    finally:
+        client.close()
