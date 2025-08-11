046a5c2c3daa870562ebdc4d0e7ea96bb297cc23
Ian-Ribeiro <ianmaxrodrigues27@gmail.com>
2025-08-11 16:37:07 -0300

    atualizando de novo

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient # Importando a classe MongoClient.
-from config import *
+from config import uri,db_name
 
 # Conectando com o MongoDB Atlas.
 def get_database(uri:str,db_name:str):
-    client = MongoClient(uri)
-    return client[db_name]
+    try:
+        client = MongoClient(uri)
+        return client[db_name]
+    except Exception as e:
+        raise ConnectionError(f"Erro ao conectar ao banco: {e}")
