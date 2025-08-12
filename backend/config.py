3c3e3351401db42f1915c97a127c31f7d33ed591
Luisinho-PT <luismiguelsousa48@gmail.com>
2025-08-12 00:23:04 -0300

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-uri = os.getenv("MONGODB_URI")
-db_name = os.getenv("MONGODB_DB", "Mercado")
--- backend/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from config import uri,db_name
-
-def get_database(uri:str,db_name:str):
-    try:
-        client = MongoClient(uri)
-        return client[db_name]
-    except Exception as e:
-        raise ConnectionError(f"Erro ao conectar ao banco: {e}")
